b4c925f28a6391f88bb727512c1df2c681a169ac
shibila1995 <shibilanaf@gmail.com>
2020-12-05 20:09:37 +0400

    edited with css

--- index.php
+++ index.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register and login</title>
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
-<body>
+<body class="backcolor">
     <div class="container">
         <div class="row">
             <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
-                <h1>Login form</h1>
+                <center><h1>Login form</h1></center>
                 <table class="table">
                     <tr>
                         <td>Email</td>
